6934f0dd362b1659282e6e0ecb4e04820f10aacd
akash-gopal <akashsingh.gopal2@sky.uk>
2022-08-02 16:58:29 +0100

    Displaying Non Direct Flights

--- src/Flights/Flights.js
+++ src/Flights/Flights.js
@@ -30,20 +30,64 @@ const Flights = ({nearestDepartureAirports, nearestArrivalAirports}) => {
             const title = document.querySelector("#title");
             title.innerHTML = "Flight Data";
             dataTitle.style.display = "block";
+            let numberOfRows = -1;
             for (let i = 0; i < flights.length; i++) {
-                const row = flightTable.insertRow(-1)
-                row.setAttribute('data-href', '#');
-                let j = 0;
-                for (const key in flights[i]) {
-                    // if (i !== 0) {
-                    const cell = row.insertCell(j);
-                    cell.innerHTML = flights[i][key];
-                    // }
-                    j++;
+                        const row = flightTable.insertRow(-1)
+                        numberOfRows ++;
+                        row.setAttribute('data-href', '#');
+                        const expand = row.insertCell(0);
+                        expand.innerHTML = "<i class='arrow'></i>"
+                        for (let j = 1; j < 6; j++) {
+                            switch (j) {
+                                case 1:
+                                    const arr = row.insertCell(j);
+                                    arr.innerHTML = flights[i]['airports'][0];
+                                    break;
+                                case 2:
+                                    const arrT = row.insertCell(j);
+                                    arrT.innerHTML = flights[i]['times'][0];
+                                    break;
+                                case 3:
+                                    const dep = row.insertCell(j);
+                                    dep.innerHTML = flights[i]['airports'][flights[i]['airports'].length - 1];
+                                    break;
+                                case 4:
+                                    const depT = row.insertCell(j);
+                                    depT.innerHTML = flights[i]['times'][flights[i]['times'].length - 1];
+                                    break;
+                                case 5:
+                                    const price = row.insertCell(j);
+                                    price.innerHTML = "£" + flights[i]['price'] + "0";
+                                    break;
+                            }
+                        }
+                        if (flights[i]['airports'].length > 2) {
+                            let numberOfChildRows = 0;
+                            for (let j = 0; j < flights[i]['airports'].length - 1; j++) {
+                                const childRow = flightTable.insertRow(-1)
+                                numberOfChildRows ++;
+                                childRow.setAttribute('data-href', '#');
+                                childRow.setAttribute('name', numberOfRows + 'child')
+                                childRow.setAttribute("style", "display:none")
+
+                                childRow.insertCell(0)
+                                const cell1 = childRow.insertCell(1);
+                                cell1.innerHTML = flights[i]['airports'][j]
+                                const cell2 = childRow.insertCell(2);
+                                cell2.innerHTML = flights[i]['times'][j * 2]
+                                const cell3 = childRow.insertCell(3);
+                                cell3.innerHTML = flights[i]['airports'][j + 1]
+                                const cell4 = childRow.insertCell(4);
+                                cell4.innerHTML = flights[i]['times'][j * 2 + 1]
+                                childRow.insertCell(5)
+                            }
+                            numberOfRows += numberOfChildRows;
+                        }
                 }
-            }
+
             makeRowsClickable();
         } catch (e) {
+            console.log(e);
             flightData.style.display = "none";
             dataTitle.style.display = "none";
             noFlights();
@@ -51,8 +95,10 @@ const Flights = ({nearestDepartureAirports, nearestArrivalAirports}) => {
     }
 
     const noFlights = () => {
+        const flightData = document.querySelector("#flightData");
         const dataTitle = document.querySelector("#dataTitle");
         const title = document.querySelector("#title");
+        flightData.style.display = "none";
         dataTitle.style.display = "block";
         title.innerHTML = "No Flights Available";
     }
@@ -70,6 +116,12 @@ const Flights = ({nearestDepartureAirports, nearestArrivalAirports}) => {
 
         const getNearestAirports = () => {
             if (nearestDepartureAirports[counter] !== nearestArrivalAirports[0]) {
+                const flightData = document.querySelector("#flightData");
+                const dataTitle = document.querySelector("#dataTitle");
+                const title = document.querySelector("#title");
+                flightData.style.display = "none";
+                dataTitle.style.display = "block";
+                title.innerHTML = "Searching For Flights...";
                 console.log(BACKEND_ADDRESS + "/flights?date=2022-11-01&dep=" + nearestDepartureAirports[counter] + "&arr=" + nearestArrivalAirports[0])
                 axios.get(BACKEND_ADDRESS + "/flights?date=2022-11-01&dep=" + nearestDepartureAirports[counter] + "&arr=" + nearestArrivalAirports[0], {
                     headers: {
@@ -79,6 +131,7 @@ const Flights = ({nearestDepartureAirports, nearestArrivalAirports}) => {
                     console.log(response);
                     if (response['data'] !== '') {
                         setFlights(response['data']);
+                        fillInForm();
                     } else {
                         const flightTable = document.querySelector("#flightTable");
                         flightTable.innerHTML = "";
@@ -132,12 +185,12 @@ const Flights = ({nearestDepartureAirports, nearestArrivalAirports}) => {
                         <div id="flightData" className={flightStyles.tableFixHead} style={{display: "none"}}>
                             <table id="table">
                                 <thead>
+                                <th></th>
                                 <th>Departure Airport</th>
                                 <th>Departure Time</th>
                                 <th>Arrival Airport</th>
                                 <th>Arrival Time</th>
-                                <th>Airline</th>
-                                <th>Flight Number</th>
+                                <th>Price</th>
                                 </thead>
                                 <tbody id="flightTable"></tbody>
                             </table>
--- src/Flights/FlightsUtils.js
+++ src/Flights/FlightsUtils.js
@@ -28,23 +28,92 @@ export function makeRowsClickable() {
     const table = document.getElementById("table");
     const rows = table.getElementsByTagName("tr");
     for (let i = 0; i < rows.length; i++) {
-        const currentRow = table.rows[i];
-        const createClickHandler = function() {
-            return function(event) {
-                const isClicked = event.target.parentElement.classList.contains('clicked');
-                const clickedItems = document.getElementsByClassName('clicked');
-                for (let i = 0; i < clickedItems.length; i++) {
-                    clickedItems[i].classList.remove('clicked');
+        if (table.rows[i] != null) {
+            if (!table.rows[i].classList.contains('childTableRow')) {
+                const currentRow = table.rows[i];
+                const createClickHandler = function () {
+                    return function (event) {
+                        console.log(event.target)
+                        let isClicked;
+                        if (event.target.tagName === "I") {
+                            isClicked = event.target.parentElement.parentElement.classList.contains('clicked');
+                        } else if (event.target.tagName === "P") {
+                            isClicked = event.target.parentElement.parentElement.parentElement.classList.contains('clicked');
+                        } else {
+                            isClicked = event.target.parentElement.classList.contains('clicked');
+                        }
+
+                        const clickedItems = document.getElementsByClassName('clicked');
+                        for (let i = 0; i < clickedItems.length; i++) {
+                            clickedItems[i].classList.remove('clicked');
+                        }
+
+                        const expandedItems = document.getElementsByClassName('expanded');
+                        for (let i = 0; i < expandedItems.length; i++) {
+                            expandedItems[i].classList.add('standard');
+                            expandedItems[i].classList.remove('expanded');
+                        }
+                        const downItems = document.getElementsByClassName('down');
+                        for (let i = 0; i < downItems.length; i++) {
+                            downItems[i].classList.remove('down');
+                        }
+
+                        if (isClicked) {
+                            if (event.target.tagName === "I") {
+                                table.rows[event.target.parentElement.parentElement.rowIndex + 1].style.display = "none";
+                                event.target.classList.remove('down');
+                                event.target.parentElement.parentElement.classList.remove('clicked');
+                                const name = event.target.parentElement.parentElement.rowIndex + 'child';
+                                const children = document.getElementsByName(name);
+                                for (let child in children) {
+                                    if (children[child].tagName !== undefined) {
+                                        children[child].classList.toggle('show');
+                                        children[child].style.display = 'none';
+                                    }
+                                }
+                            } else {
+                                event.target.parentElement.classList.remove('clicked');
+                                const name = event.target.parentElement.rowIndex + 'child';
+                                const children = document.getElementsByName(name);
+                                for (let child in children) {
+                                    if (children[child].tagName !== undefined) {
+                                        children[child].classList.toggle('show');
+                                        children[child].style.display = 'none';
+                                    }
+                                }
+                                event.target.parentElement.children[0].children[0].classList.remove('down');
+                            }
+                        } else {
+                            if (event.target.tagName === "I") {
+                                event.target.classList.add('down');
+                                event.target.parentElement.parentElement.classList.add('clicked');
+                                const name = event.target.parentElement.parentElement.rowIndex + 'child';
+                                const children = document.getElementsByName(name);
+                                for (let child in children) {
+                                    if (children[child].tagName !== undefined) {
+                                        children[child].classList.toggle('show');
+                                        children[child].style.display = 'table-row';
+                                    }
+                                }
+                            } else {
+                                event.target.parentElement.children[0].children[0].classList.add('down');
+                                event.target.parentElement.classList.add('clicked');
+                                const name = event.target.parentElement.rowIndex + 'child';
+                                const children = document.getElementsByName(name);
+                                for (let child in children) {
+                                    if (children[child].tagName !== undefined) {
+                                        children[child].classList.toggle('show');
+                                        children[child].style.display = 'table-row';
+                                    }
+                                }
+                            }
+                        }
+                        showConfirmButton();
+                    }
                 }
-                if (isClicked) {
-                    event.target.parentElement.classList.remove('clicked');
-                } else {
-                    event.target.parentElement.classList.toggle('clicked');
-                }
-                showConfirmButton();
-            };
-        };
-        currentRow.onclick = createClickHandler();
+                currentRow.onclick = createClickHandler();
+            }
+        }
     }
 }
 
