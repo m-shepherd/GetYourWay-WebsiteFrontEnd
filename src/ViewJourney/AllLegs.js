0778f09b4783d4c800baca4a06e85066e5a0a51a
Dan Charlton <dlcharlton14@gmail.com>
2022-08-03 16:26:39 +0100

    Fixed header size in relation to table

--- src/ViewJourney/AllLegs.js
+++ src/ViewJourney/AllLegs.js
@@ -24,6 +24,7 @@ class AllLegs extends Component {
                             <th>End</th>
                             <th>EndTime</th>
                             <th>duration</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
